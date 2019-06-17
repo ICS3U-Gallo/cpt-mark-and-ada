6bbb7700663b0d006fd0203fa6de1f807824a341
Satori <34246977+cnshuimu@users.noreply.github.com>
2019-06-17 10:24:02 -0400

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -3,32 +3,35 @@ from apple import Apple
 
 
 class Snake:
+    # define the initials
     def __init__(self):
-
         self.body = []
         self.length = 0
         self.tail = []
         self.add_body(4, 4)
-        self.score = 0
         self.add_body(3, 4)
         self.add_body(2, 4)
+        self.score = 0
 
+    # add snake body
     def add_body(self, row, column):
         self.body.append([row, column])
         self.length += 1
         self.tail = self.body[-1]
 
     def update(self, direction):
-
+        # determine wheather snake is died or not
         if self.defeat(direction):
             return True
 
         self.tail = [self.body[-1][0], self.body[-1][1]]
 
+        # snake moving
         for i in range(self.length - 1, 0, -1):
             self.body[i][0] = self.body[i - 1][0]
             self.body[i][1] = self.body[i - 1][1]
 
+        # change snake's direction
         if direction == UP:
             self.body[0][0] += 1
         elif direction == DOWN:
@@ -41,8 +44,11 @@ class Snake:
         return False
 
     def defeat(self, direction):
+        # snake died if eat itself
         if self.body[0] in self.body[1:]:
             return True
+
+        # snake died if go out of boundary
         elif direction == UP and self.body[0][0] >= COLUMN_COUNT - 1:
             return True
         elif direction == DOWN and self.body[0][0] <= 0:
@@ -54,10 +60,11 @@ class Snake:
 
         return False
 
+    # if snake eat an apple, increase its body length by 1 grid
     def eat_apple(self, apple):
         if self.body[0] == [apple.x, apple.y]:
             self.length += 1
             self.body.append(self.tail)
             self.score += apple.score
 
-            apple.prodece(self)
+            apple.produce(self)
